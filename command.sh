a14e6f03f64d6d3054ff81fbd55b793b8b2b7df0
Jonathan Marques <jonathan.marques.costa@outlook.com>
2023-05-18 15:21:30 -0300

    dados

---
+++ command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for counter in $(seq 1 1000000); do  
+  for counter in $(seq 1 1000000); do
+    echo facebook.com
+  done
+done
+
+# for counter in $(seq 1 255); do echo "$counter"; done
